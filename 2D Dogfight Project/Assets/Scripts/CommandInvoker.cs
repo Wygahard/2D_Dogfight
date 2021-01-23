66b2e6466e768ee909a2f8826f0edd7453203732
Wygahard <Wygahard@hotmail.fr>
2021-01-23 10:34:17 +0000

    Set up of Command System

---
+++ 2D Dogfight Project/Assets/Scripts/CommandInvoker.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommandInvoker : MonoBehaviour
+{
+    static Queue<ICommand> commandBuffer;
+    static List<ICommand> commandHistory;
+    static int counter;
+
+    private void Awake()
+    {
+        commandBuffer = new Queue<ICommand>();
+        commandHistory = new List<ICommand>();
+    }
+
+    public static void AddCommand(ICommand command)
+    {
+        commandBuffer.Enqueue(command);
+    }
+
+    private void Update()
+    {
+        if(commandBuffer.Count > 0)
+        {
+            ICommand c = commandBuffer.Dequeue();
+            c.Execute();
+            //commandBuffer.Dequeue().Execute();
+
+            commandHistory.Add(c);
+            counter++;
+        }
+    }
+}
